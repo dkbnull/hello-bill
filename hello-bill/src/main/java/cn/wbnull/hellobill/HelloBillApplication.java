f039be3fd138834f27a454dccf2917a5d9d7cfc1
dkbnull <dkbnull@163.com>
2020-12-28 23:39:47 +0800

    框架

---
+++ hello-bill/src/main/java/cn/wbnull/hellobill/HelloBillApplication.java
@@ -0,0 +1,18 @@
+package cn.wbnull.hellobill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 入口类
+ *
+ * @author dukunbiao(null)  2020-12-28
+ */
+@SpringBootApplication
+public class HelloBillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloBillApplication.class, args);
+    }
+}
+
