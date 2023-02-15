162506a70c1279e997adb7454e96becc0c6adf5f
dkbnull <dkbnull@163.com>
2023-02-15 21:38:20 +0800

    导航页增加选项卡

--- hello-bill/src/main/resources/static/js/home.js
+++ hello-bill/src/main/resources/static/js/home.js
@@ -4,16 +4,51 @@
  * @author dukunbiao(null)  2020-12-29
  * https://github.com/dkbnull/HelloBill
  */
+let $;
+
 layui.use(['layer', 'element'], function () {
     if (!validate()) {
         return;
     }
 
-    const $ = layui.jquery;
-    $(".username").html(getItem("username"));
+    $ = layui.jquery;
+    $('.username').html(getItem('username'));
+
+    initMethod();
 });
+
+function initMethod() {
+    $('.layui-nav-child a').on('click', function () {
+        const method = $(this).data('method');
+        active[method] ? active[method].call(this) : '';
+    });
+
+    const active = {
+        openTab: function () {
+            const id = $(this).data('id');
+            const title = $(this).data('title');
+            const href = $(this).data('href');
+
+            openTab(id, title, href)
+        }
+    };
+}
+
+function openTab(id, title, href) {
+    const element = layui.element
+    let node = $('li[lay-id="' + id + '"]');
+    if (node.length === 0) {
+        element.tabAdd('tab', {
+            id: id,
+            title: title,
+            content: '<iframe src="' + href + '" id="frame" class="frame"></iframe>'
+        })
+    }
+
+    element.tabChange('tab', id);
+}
 
 function logout() {
     localStorage.clear();
-    window.location.href = "index.html";
+    window.location.href = 'index.html';
 }
